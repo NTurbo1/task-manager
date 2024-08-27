1a944f7f7a55c34cb40a158ebebbf291456ae597
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2024-08-27 23:54:51 +0500

    Project initialization

---
+++ src/main/java/com/nturbo1/task_management_api/TaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nturbo1.task_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nturbo1/task_management_api/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nturbo1.task_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
