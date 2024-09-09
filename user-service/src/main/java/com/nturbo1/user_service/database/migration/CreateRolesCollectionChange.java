f156afe4bc906c639a89079c568481b5a9195e3c
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2024-09-09 22:18:47 +0500

    Added create roles collection mongock change unit.

---
+++ user-service/src/main/java/com/nturbo1/user_service/database/migration/CreateRolesCollectionChange.java
@@ -0,0 +1,25 @@
+package com.nturbo1.user_service.database.migration;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@RequiredArgsConstructor
+@ChangeUnit(id = "create-roles-collection", order = "002", author = "nturbo1")
+public class CreateRolesCollectionChange {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Execution
+    public void createRolesCollection() {
+        mongoTemplate.createCollection("roles");
+    }
+
+    @RollbackExecution
+    public void rollback() {
+        mongoTemplate.dropCollection("roles");
+    }
+}
+
