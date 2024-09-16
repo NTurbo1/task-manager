fcc0a040ca590fdbbbae15d7a723a2cb809d2f45
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2024-09-16 15:51:38 +0500

    Added swagger/openAPI configs to the api gateway server module.

---
+++ api-gateway/src/main/java/com/nturbo1/api_gateway/config/doc/OpenApiConfigs.java
@@ -0,0 +1,23 @@
+package com.nturbo1.api_gateway.config.doc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.ws.rs.HttpMethod;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0.0", description = "Documentation API Gateway v1.0.0"))
+public class OpenApiConfigs {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/user-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb:http://USER-SERVICE"))
+                .build();
+    }
+
+}
