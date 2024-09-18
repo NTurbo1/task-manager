f597f65ed4fb73441d3f1b02962686c8f42cd6b5
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2024-09-18 20:54:14 +0500

    Added cors configs in the api gateway module.

---
+++ api-gateway/src/main/java/com/nturbo1/api_gateway/config/cors/CustomCorsConfigs.java
@@ -0,0 +1,28 @@
+package com.nturbo1.api_gateway.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CustomCorsConfigs extends CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter()
+    {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials( true );
+        config.setAllowedOriginPatterns(List.of( "*" ));
+        config.setAllowedMethods( List.of( "GET", "POST", "PUT", "DELETE", "OPTIONS" ) );
+        config.setAllowedHeaders( List.of( "origin", "content-type", "accept", "cookie" ) );
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", config );
+
+        return new CorsWebFilter(source);
+    }
+}
